6d2e43c294941827548c161f998b034209bb103d
Richard Dallaway <richard@dallaway.com>
2019-09-25 13:48:45 +0100

    find most recent class for value in column

--- src/oner.rs
+++ src/oner.rs
@@ -1,5 +1,6 @@
 use super::dataset::*;
 use itertools::Itertools;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Rule {
@@ -22,18 +23,71 @@ fn distinct_column_values(
         .collect()
 }
 
-/*fn most_frequent_class(dataset: &Dataset<AttributeName, Example>, attribute_index: usize, value: &Value) -> &Value {
-    let classes = dataset.examples.iter().filter(|row| &row.attribute_values[attribute_index] == value).map(|row| row.class);
-    // find most frequent value
-}*/
+fn most_frequent_class<'d>(
+    dataset: &'d Dataset<AttributeName, Example>,
+    attribute_index: usize,
+    value: &str,
+) -> Option<&'d Value> {
+    let classes = dataset
+        .examples
+        .iter()
+        .filter(|row| row.attribute_values[attribute_index] == value)
+        .map(|row| &row.class);
+
+    let mut counts = HashMap::new();
+    for class in classes {
+        *counts.entry(class).or_insert(0) += 1;
+    }
+
+    counts
+        .into_iter()
+        .max_by_key(|&(_, count)| count)
+        .map(|(class, _)| class)
+}
+
+#[cfg(test)]
+mod test_freq_class {
+    use super::most_frequent_class;
+    use super::Dataset;
+    use super::Example;
+    #[test]
+    fn find_most_frequent_class() {
+        let dataset = Dataset {
+            input_attribute_names: vec![String::from("x")],
+            output_attribute_name: String::from("y"),
+            examples: vec![
+                Example {
+                    attribute_values: vec![String::from("yes")],
+                    class: String::from("lo"),
+                },
+                Example {
+                    attribute_values: vec![String::from("noo")],
+                    class: String::from("lo"),
+                },
+                Example {
+                    attribute_values: vec![String::from("yes")],
+                    class: String::from("hi"),
+                },
+                Example {
+                    attribute_values: vec![String::from("yes")],
+                    class: String::from("hi"),
+                },
+            ],
+        };
+        assert_eq!(
+            most_frequent_class(&dataset, 0, "yes"),
+            Some(&String::from("hi"))
+        );
+    }
+}
 
 pub fn discover(dataset: &Dataset<AttributeName, Example>) -> Rule {
     // let hypotheses = Vec::new();
 
     for (a_index, a_name) in dataset.input_attribute_names.iter().enumerate() {
         for v in distinct_column_values(dataset, a_index) {
-            // let class = dataset.max_class(a_index, v);
-            println!("{} {} {}", a_index, a_name, &v);
+            let class = most_frequent_class(dataset, a_index, &v);
+            println!("{} IF {} = {} THEN {:?}", a_index, a_name, &v, &class);
         }
     }
 
